2652eacab6f2820864f949288b195294525af447
lgh7418 <lgh7418@gmial.com>
2020-07-29 14:04:07 +0900

    graphql 기초

---
+++ db/db.js
@@ -0,0 +1,50 @@
+let movies = [
+  {
+    id: 0,
+    name: "Star wars - The new one",
+    score: 1,
+  },
+  {
+    id: 1,
+    name: "Avengers - The new one",
+    score: 8,
+  },
+  {
+    id: 2,
+    name: "The Gotfather",
+    score: 99,
+  },
+  {
+    id: 3,
+    name: "Lgan",
+    score: 2,
+  },
+];
+
+export const getMovies = () => movies;
+
+export const getById = (id) => {
+  const filteredMovies = movies.filter((movie) => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  console.log(movies);
+  return newMovie;
+};
