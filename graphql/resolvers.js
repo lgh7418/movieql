0c23dcf7dc20a2ab6e14992d7708eae272785a52
lgh7418 <lgh7418@gmial.com>
2020-07-29 15:06:04 +0900

    final API (axios로 변경, api url 추가 등)

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,10 @@
-import { getMovies } from "../db/db";
+import { getMovies, getMovie, getSuggestions } from "../db/db";
 
 const resolvers = {
   Query: {
     movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id),
   },
 };
 
