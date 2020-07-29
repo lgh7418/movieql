b96b9ce8767fd4cb372fc5c3bca3f3f4d5ca7ba0
lgh7418 <lgh7418@gmial.com>
2020-07-29 14:42:36 +0900

    Get all movies

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,8 @@
-import { getMovies, getById, addMovie, deleteMovie } from "../db/db";
+import { getMovies } from "../db/db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id),
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id),
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
   },
 };
 
